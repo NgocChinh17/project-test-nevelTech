f1ffb1a2d7ac8db52748a285fc4cdb1a2375e518
NgocChinh17 <chinhwedit@gmail.com>
2025-04-14 08:59:03 +0700

    fix capacity image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import "./globals.css";
 import { Inter } from 'next/font/google';
+
 const inter = Inter({ subsets: ['latin'] });
 
+export const metadata = {
+  title: 'bài test 3',
+};
 
 export default function RootLayout({
   children,
@@ -10,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{ backgroundColor: "#03193A",}}>
+      <body className={inter.className} style={{ backgroundColor: "#03193A" }}>
         {children}
       </body>
     </html>
